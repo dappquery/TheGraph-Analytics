60dabc63884c3ed2202e42e02b7fbc1ade662377
tech-nerd <abhinav@Abhinavs-MacBook-Pro.local>
2021-06-16 22:47:23 +0530

    tabs fix

--- src/app/dashboard/thegraph/thegraph.component.ts
+++ src/app/dashboard/thegraph/thegraph.component.ts
@@ -15,37 +15,55 @@ export class ThegraphComponent implements OnInit {
   dashboardUrl: string;
   constants = Constants;
   isTab4Active: boolean;
-  isMoreTabOpen = false;
   arrayOfAaddresses = [];
-  labels = this.constants.LABELS.theGraphDashboard;
+  labels = Constants.LABELS.theGraphDashboard;
+
   constructor(
     private readonly router: Router,
     private readonly activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    //localhost:4200/indexer?indexer_address=0x7697a886fc3b71a8a88487019337a6bbe5838f1a
+    // http: //localhost:4200/delegator?id=0xebc391ba182f6d8654a516a44e382cf9c9196831
+
     this.routeTodashboard();
   }
 
   routeTodashboard() {
-    let route = this.router.url.replace(/\\/g, '');
+    let route;
+    if (this.router.url.includes('delegator')) {
+      route = 'delegator';
+    } else if (this.router.url.includes('indexer')) {
+      route = 'indexer';
+    } else if (this.router.url.includes('curator')) {
+      route = 'curator';
+    }
+    // route = this.router.url.includes('delegator') ? 'delegator' : 'indexer';
     switch (route) {
-      case this.labels.indexer:
+      case 'indexer':
         {
           this.switchTab('tab1');
+          this.arrayOfAaddresses =
+            this.activatedRoute.snapshot.queryParams['indexer_address'];
+          if (this.activatedRoute.snapshot.queryParams['indexer_address']) {
+            this.arrayOfAaddresses =
+              this.activatedRoute.snapshot.queryParams['indexer_address'];
+            this.switchTab('tab1');
+          }
         }
         break;
-      case this.labels.delegator: {
+      case 'delegator': {
         this.switchTab('tab2');
+        if (this.activatedRoute.snapshot.queryParams['id']) {
+          this.arrayOfAaddresses =
+            this.activatedRoute.snapshot.queryParams['id'];
+          this.switchTab('tab2');
+        }
         break;
       }
-
-      case this.labels.curator: {
+      case 'curator': {
         this.switchTab('tab3');
-        break;
-      }
-      default: {
-        this.switchTab('tab1');
         break;
       }
     }
@@ -53,20 +71,41 @@ export class ThegraphComponent implements OnInit {
   switchTab(tab: string) {
     switch (tab) {
       case 'tab1': {
-        this.dashboardUrl = this.constants.IndexerDashboard;
-        this.disableAllTabs();
-        this.isTab1Active = true;
-        this.router.navigate([this.labels.indexer]);
+        if (this.activatedRoute.snapshot.queryParams['indexer_address']) {
+          this.dashboardUrl = this.activatedRoute.snapshot.queryParams[
+            'indexer_address'
+          ]
+            ? `${
+                this.constants.IndividualIndexerDashboard
+              }?${this.createUrlWithAddress('indexer')}`
+            : `${this.constants.IndividualIndexerDashboard}`;
+          console.log('indexer url::', this.dashboardUrl);
+          this.disableAllTabs();
+          this.isTab1Active = true;
+        } else {
+          this.dashboardUrl = this.constants.IndexerDashboard;
+          this.disableAllTabs();
+          this.isTab1Active = true;
+        }
         break;
       }
       case 'tab2': {
-        this.dashboardUrl = this.constants.DelegratorDashboard;
-        this.disableAllTabs();
-        this.isTab2Active = true;
-        this.router.navigate([this.labels.delegator]);
+        if (this.activatedRoute.snapshot.queryParams['id']) {
+          this.dashboardUrl = this.activatedRoute.snapshot.queryParams['id']
+            ? `${
+                this.constants.IndividualDelegatorDashboard
+              }?${this.createUrlWithAddress('delegator')}`
+            : this.constants.IndividualDelegatorDashboard;
+          console.log('Delegator url::', this.dashboardUrl);
+          this.disableAllTabs();
+          this.isTab4Active = true;
+        } else {
+          this.dashboardUrl = this.constants.DelegratorDashboard;
+          this.disableAllTabs();
+          this.isTab2Active = true;
+        }
         break;
       }
-
       case 'tab3': {
         this.dashboardUrl = this.constants.curatorDashboard;
         this.disableAllTabs();
@@ -87,14 +126,14 @@ export class ThegraphComponent implements OnInit {
       if (this.arrayOfAaddresses.length) {
         this.arrayOfAaddresses.forEach((address) => {
           url =
-            typeOfAddress === this.labels.indexer
+            typeOfAddress === 'indexer'
               ? url + `indexer_address=${address}&`
               : url + `id=${address}&`;
         });
       }
     } else {
       url =
-        typeOfAddress === this.labels.indexer
+        typeOfAddress === 'indexer'
           ? url + `indexer_address=${this.arrayOfAaddresses}`
           : url + `id=${this.arrayOfAaddresses}`;
     }
